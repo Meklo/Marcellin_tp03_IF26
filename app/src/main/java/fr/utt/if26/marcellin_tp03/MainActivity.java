34589cf75e1f9c0e70be909a0d40df8f065ed5ef
Meklo <marcellinhugo@orange.fr>
2017-10-03 14:11:49 +0200

    Initial commit

---
+++ app/src/main/java/fr/utt/if26/marcellin_tp03/MainActivity.java
@@ -0,0 +1,25 @@
+package fr.utt.if26.marcellin_tp03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * Cette méthode est déclenchée à la création de l'activité
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String message = "Bonjour IF26";
+
+        TextView tx = (TextView) findViewById(R.id.message);
+        tx.setText(message);
+    }
+}
